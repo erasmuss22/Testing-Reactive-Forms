a87f6aa85fbef20446786ad1b3106a7c4e331052
erasmuss22 <erasmuss22@gmail.com>
2018-02-05 22:07:48 -0600

    added first tests for form

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,27 +1,46 @@
 import { TestBed, async } from '@angular/core/testing';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        ReactiveFormsModule
+      ],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
+
+  it('should require a username', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    const component: AppComponent = fixture.componentInstance;
+    component.ngOnInit();
+    
+    expect(component.username.errors['required']).toBeDefined();
+
+    component.username.setValue('a');
+
+    expect(component.username.errors['required']).toBeUndefined();
   }));
-  it(`should have as title 'app'`, async(() => {
+
+  it('should have an error with fewer than 4 characters in a username', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    const component: AppComponent = fixture.componentInstance;
+    component.ngOnInit();
+    
+    component.username.setValue('aaa');
+    expect(component.username.errors.minlength).toBeDefined();
   }));
-  it('should render title in a h1 tag', async(() => {
+
+  it('should have an error with more than 10 characters in a username', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    const component: AppComponent = fixture.componentInstance;
+    component.ngOnInit();
+    
+    component.username.setValue('aaaaaaaaaaa');
+    expect(component.username.errors.maxlength).toBeDefined();
   }));
 });
